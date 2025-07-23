ab6c8c89cae8173f693a2540604be4e2ca172dfa
dhrupad777 <your-github-email@example.com>
2025-07-23 21:23:43 +0530

    Linear search

---
+++ linearsearch.java
@@ -0,0 +1,19 @@
+public class linearsearch {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        int target= 18;
+        int ans = search(nums,target);
+        System.out.println(ans);
+    }
+        static int search(int[] arr, int target){
+            if(arr.length == 0){
+                return -1;
+            }
+            for(int i=0;i<arr.length;i++){
+                if(target == arr[i]){
+                    return i;
+                }
+            }
+            return -1;
+        }
+}
