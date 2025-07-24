3d39ca9f1d58b10a5938e054fdb2902d7e27f879
dhrupad777 <dhrupadrajpurohit@gmail.com>
2025-07-25 01:05:51 +0530

    binary search (with correct email)

---
+++ binarysearch.java
@@ -0,0 +1,10 @@
+public class binarysearch {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,77,88,99,55,99};
+        bsearch(nums);
+    }
+    static int bsearch(int[] arr){
+        
+        return -1;
+    }
+}
