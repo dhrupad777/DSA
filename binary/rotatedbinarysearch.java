a85fa352489740253377b1a5e6fa0dfdf432371b
dhrupad777 <dhrupadrajpurohit@gmail.com>
2025-08-09 17:30:00 +0530

    Duplicates

--- binary/rotatedbinarysearch.java
+++ binary/rotatedbinarysearch.java
@@ -46,7 +46,22 @@ public class rotatedbinarysearch {
             end = mid -1;
         }else{
             start = mid +1;
-        }        
+        }   
+        if(arr[mid] == arr[start] && arr[mid] == arr[end]){
+            if(arr[start] >arr[start +1]){
+                return start;
+            }
+            start++;
+            if(arr[end] < arr[end  -1]){
+                return end -1;
+            }     
+            end--;       
+        }     
+        else if( arr[start] == arr[mid] && arr[mid] > arr[end]){
+            start = mid +1;
+        }else{
+            end = mid -1;
+        }
         }
         
        return -1; 
